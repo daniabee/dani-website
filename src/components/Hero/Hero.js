99e585795ac45462d88c9eb2331c12578c2e739d
Dani Bagley <bagley.a.danielle@gmail.com>
2023-03-23 18:49:08 -0600

    add styling to hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -3,10 +3,26 @@ import "./Hero.css";
 const Hero = () => {
   return (
     <div className="hero">
+      <div id="background-wrap">
+        <div class="bubble x1"></div>
+        <div class="bubble x2"></div>
+        <div class="bubble x3"></div>
+        <div class="bubble x4"></div>
+        <div class="bubble x5"></div>
+        <div class="bubble x6"></div>
+        <div class="bubble x7"></div>
+        <div class="bubble x8"></div>
+        <div class="bubble x9"></div>
+        <div class="bubble x10"></div>
+        <div class="bubble x11"></div>
+      </div>
       <div className="container">
+        <div className="content">
+          <h2 className="welcome">HI THERE,</h2>
+          <h2 className="welcome">HI THERE,</h2>
+        </div>
         <div className="title">
-          <h1 className="welcome">HI THERE,</h1>
-          <h1 className="greeting">I'M DANI BAGLEY.</h1>
+          <h2 className="greeting">I'M DANI BAGLEY.</h2>
         </div>
       </div>
     </div>
